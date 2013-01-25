ce11a94a4899eea9b0f66f7d50f8de38f8c3f8b8
stfn <stfnmd@gmail.com>
2013-01-25 19:35:38 +0100

    Add LICENSE

--- whatflac.pl
+++ whatflac.pl
@@ -1,7 +1,37 @@
 #!/usr/bin/perl
 
+################################################################################
+#
+# whatflac - Transcode FLAC files and create torrent.
+#
+# Copyright (C) 2013  stfn <stfnmd@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+#
+# Historically based on:
+#
+# 	- whatmp3 by shardz (logik.li)
+# 	- Flac to Mp3 Perl Converter by Somnorific
+#	- Scripts by Falkano and Nick Sklaventitis
+#
+################################################################################
+
 use strict;
 use warnings;
+
 use Cwd;
 use File::Basename qw/basename/;
 use File::Find qw/find/;
@@ -10,35 +40,28 @@ use File::Copy qw/copy/;
 use Getopt::Long;
 Getopt::Long::Configure("permute");
 
-my ($verbose, $notorrent, $zeropad, $moveother, $output, $passkey, $tracker);
+my ($OPT_VERBOSE, $OPT_NOTORRENT, $OPT_ZEROPAD, $OPT_MOVEOTHER, $OPT_OUTPUT, $OPT_PASSKEY, $OPT_TRACKER);
 
-##############################################################
-# whatmp3 - Convert FLAC to mp3, create what.cd torrent.
-# Created by shardz (logik.li)
-# Based on: Flac to Mp3 Perl Converter by Somnorific
-# Which was based on: Scripts by Falkano and Nick Sklaventitis
-##############################################################
-
-my $VERSION = "2.3.2";
+my $VERSION = "1.0";
 
 # Do you always want to move additional files (.jpg, .log, etc)?
-$moveother = 1;
+$OPT_MOVEOTHER = 0;
 
 # Output folder unless specified: ("/home/samuel/Desktop/")
-$output = "";
+$OPT_OUTPUT = "";
 
 # Do you want to zeropad tracknumber values? (1 => 01, 2 => 02 ...)
-$zeropad = 1;
+$OPT_ZEROPAD = 1;
 
 # Specify torrent passkey
-$passkey = "";
+$OPT_PASSKEY = "";
 
 # Specify tracker ("http://tracker.what.cd:34000/")
-$tracker = "http://tracker.what.cd:34000/";
+$OPT_TRACKER = "http://tracker.what.cd:34000/";
 
 # List of default encoding options, add to this list if you want more
 my %enc_options = (
-  "320"  => {enc => "lame", opts => "-b 320 --ignore-tag-errors"},
+	"320"  => {enc => "lame", opts => "-b 320 --ignore-tag-errors"},
 	"V0"   => {enc => "lame", opts => "-V 0 --vbr-new --ignore-tag-errors"},
 	"V2"   => {enc => "lame", opts => "-V 2 --vbr-new --ignore-tag-errors"},
 	"Q8"   => {enc => "oggenc", opts => "-q 8"},
@@ -51,7 +74,7 @@ my %enc_options = (
 
 sub help {
 	print<<__EOH__;
-whatmp3 version $VERSION
+whatflac version $VERSION
 
 Usage of $0:
 	--320 --V2 --V0 --Q8 --ALAC ...
@@ -76,7 +99,7 @@ Usage of $0:
 Minimally, you need a passkey, a tracker, and an encoding option to create a 
 working torrent to upload.
 
-whatmp3 depends on metaflac, lame/oggenc, and mktorrent.
+whatflac depends on flac, metaflac, lame/oggenc, and mktorrent.
 __EOH__
 exit;
 }
@@ -98,10 +121,10 @@ sub process {
 	push(@flac_dirs, $arg);
 }
 
-GetOptions('help' => \&help, 'verbose' => \$verbose, 'notorrent' => \$notorrent, 'zeropad', => \$zeropad, 'moveother' => \$moveother, 'output=s' => \$output, 'passkey=s' => \$passkey, 'tracker=s' => \$tracker, '<>' => \&process);
+GetOptions('help' => \&help, 'verbose' => \$OPT_VERBOSE, 'notorrent' => \$OPT_NOTORRENT, 'zeropad', => \$OPT_ZEROPAD, 'moveother' => \$OPT_MOVEOTHER, 'output=s' => \$OPT_OUTPUT, 'passkey=s' => \$OPT_PASSKEY, 'tracker=s' => \$OPT_TRACKER, '<>' => \&process);
 
-$output ||= "./";
-$output =~ s'/?$'/' if $output;	# Add a missing /
+$OPT_OUTPUT ||= "./";
+$OPT_OUTPUT =~ s'/?$'/' if $OPT_OUTPUT;	# Add a missing /
 
 unless (@flac_dirs) {
 	print "Need FLAC file parameter\n";
@@ -120,14 +143,14 @@ foreach my $flac_dir (@flac_dirs) {
 	}
 	find( sub { push(@files, $File::Find::name) if ($File::Find::name =~ m/\.flac$/i) }, $flac_dir);
 	
-	print "Using $flac_dir\n" if $verbose;
+	print "Using $flac_dir\n" if $OPT_VERBOSE;
 	
 	foreach my $enc_option (@enc_options) {
-		my $mp3_dir = $output . basename($flac_dir) . " ($enc_option)";
+		my $mp3_dir = $OPT_OUTPUT . basename($flac_dir) . " ($enc_option)";
 		$mp3_dir =~ s/FLAC//ig;
 		mkpath($mp3_dir);
 		
-		print "\nEncoding with $enc_option started...\n" if $verbose;
+		print "\nEncoding with $enc_option started...\n" if $OPT_VERBOSE;
 	
 		foreach my $file (@files) {
 			$file =~ s/\$/\\\$/g;	# fix error with bash and $'s
@@ -143,7 +166,7 @@ foreach my $flac_dir (@flac_dirs) {
 				$tags{$tag} =~ s/\"/\\\"/g;	# fix error with escaping "'s
 			}
 			
-			$tags{'TRACKNUMBER'} =~ s/^(?!0|\d{2,})/0/ if $zeropad;	# 0-pad tracknumbers, if desired.
+			$tags{'TRACKNUMBER'} =~ s/^(?!0|\d{2,})/0/ if $OPT_ZEROPAD;	# 0-pad tracknumbers, if desired.
 	
 			if ($tags{'TRACKNUMBER'} and $tags{'TITLE'}) {
 				$mp3_filename = $mp3_dir . '/' . $tags{'TRACKNUMBER'} . " - " . $tags{'TITLE'};
@@ -184,14 +207,14 @@ foreach my $flac_dir (@flac_dirs) {
 					$mp3_filename . '.m4a" 2>&1';
 			}
 			$flac_command =~ s/\$/\\\$/g;	# fix error with bash and $'s
-			print "$flac_command\n" if $verbose;
+			print "$flac_command\n" if $OPT_VERBOSE;
 			system($flac_command);
 		}
 	
 		print "\nEncoding with $enc_option finished...\n";
 	
-		if ($moveother) {
-			print "Moving other files... " if $verbose;
+		if ($OPT_MOVEOTHER) {
+			print "Moving other files... " if $OPT_VERBOSE;
 		
 			my $base_mp3_dir = basename($mp3_dir);
 			find( { wanted => sub { 
@@ -206,12 +229,12 @@ foreach my $flac_dir (@flac_dirs) {
 			}, no_chdir => 1 }, $flac_dir);
 		}
 	
-		if ($output and $passkey and $tracker and not $notorrent) {
-			print "\nCreating torrent... " if $verbose;
-			my $torrent_create = 'mktorrent -p -a ' . $tracker . $passkey . '/announce -o "' . $output . basename($mp3_dir) . '.torrent" "' . $mp3_dir . '"';
-			print "'$torrent_create'\n" if $verbose;
+		if ($OPT_OUTPUT and $OPT_PASSKEY and $OPT_TRACKER and not $OPT_NOTORRENT) {
+			print "\nCreating torrent... " if $OPT_VERBOSE;
+			my $torrent_create = 'mktorrent -p -a ' . $OPT_TRACKER . $OPT_PASSKEY . '/announce -o "' . $OPT_OUTPUT . basename($mp3_dir) . '.torrent" "' . $mp3_dir . '"';
+			print "'$torrent_create'\n" if $OPT_VERBOSE;
 			system($torrent_create);
 		}
 	}
-	print "\nAll done with $flac_dir...\n" if $verbose;
+	print "\nAll done with $flac_dir...\n" if $OPT_VERBOSE;
 }
